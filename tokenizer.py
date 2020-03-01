c7ef06820ce5c02c52780f7a8e9228c3a74e071f
Kavitha Rajan <KavithaRajan@users.noreply.github.com>
2020-03-01 11:52:34 +0530

    Create tokenizer.py

---
+++ tokenizer.py
@@ -0,0 +1,22 @@
+# How to tokenize the input text using NLTK
+
+# import the following packages:
+from nltk.tokenize import sent_tokenize, word_tokenize, WordPunctTokenizer
+
+# Define some input text that will be used for tokenization:
+input_text = "Do you know how tokenization works? It's actually quite interesting! Let's analyze a couple of sentences and figure it out."
+
+#Divide the input text into sentence tokens:
+# Sentence tokenizer
+print("\nSentence tokenizer:")
+print(sent_tokenize(input_text))
+
+#Divide the input text into word tokens:
+# Word tokenizer
+print("\nWord tokenizer:")
+print(word_tokenize(input_text))
+
+#Divide the input text into word tokens using word punct tokenizer:
+# WordPunct tokenizer
+print("\nWord punct tokenizer:")
+print(WordPunctTokenizer().tokenize(input_text))
